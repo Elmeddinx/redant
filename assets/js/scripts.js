efcc35ec3e4cd9aa1cc97c96938d9abb55f53206
Elmeddin Xudaverdiyev <Xudaverdiyev11@gmail.com>
2023-09-14 12:24:08 +0400

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,60 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
     slidesPerView: "auto",
+    pagination: false,
+    // spaceBetween: 30,
     breakpoints: {
         320: {
+            slidesPerView: 1.4,
+        },
+        375: {
+            slidesPerView: 1.65,
+        },
+        450: {
             slidesPerView: 2,
         },
-        1024: {
+        525: {
+            slidesPerView: 2.3,
+        },
+        625: {
+            slidesPerView: 2.8,
+        },
+        725: {
+            slidesPerView: 3.25,
+        },
+        825: {
+            slidesPerView: 3.75,
+        },
+        900: {
+            slidesPerView: 4,
+        },
+        1200: {
             slidesPerView: 6,
         },
     },
 });
 
-
-window.addEventListener("resize", () => {
-    var genislik = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    if (genislik <= 768) {
-        var lineAttr = document.querySelectorAll(".swiperLine");
-        lineAttr.forEach(e => {
-            e.querySelector("line").setAttribute("x2", "35");
-        });
-        var circleAttr = document.querySelectorAll(".swiperLine");
-        circleAttr.forEach(e => {
-            e.querySelector("circle").setAttribute("cx", "45");
-        });
-    } else if (genislik >= 768) {
-        var lineAttr = document.querySelectorAll(".swiperLine");
-        lineAttr.forEach(e => {
-            e.querySelector("line").setAttribute("x2", "108");
-        });
-        var circleAttr = document.querySelectorAll(".swiperLine");
-        circleAttr.forEach(e => {
-            e.querySelector("circle").setAttribute("cx", "122");
-        });
-    }
+var eventList = ["resize", "load"];
+eventList.forEach(e => {
+    window.addEventListener(e, () => {
+        var genislik = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (genislik <= 1200) {
+            var lineAttr = document.querySelectorAll(".swiperLine");
+            lineAttr.forEach(e => {
+                e.querySelector("line").setAttribute("x2", "30");
+            });
+            var circleAttr = document.querySelectorAll(".swiperLine");
+            circleAttr.forEach(e => {
+                e.querySelector("circle").setAttribute("cx", "40");
+            });
+        } else if (genislik >= 1200) {
+            var lineAttr = document.querySelectorAll(".swiperLine");
+            lineAttr.forEach(e => {
+                e.querySelector("line").setAttribute("x2", "108");
+            });
+            var circleAttr = document.querySelectorAll(".swiperLine");
+            circleAttr.forEach(e => {
+                e.querySelector("circle").setAttribute("cx", "122");
+            });
+        }
+    });
 });
