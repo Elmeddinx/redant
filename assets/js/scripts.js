208f025de93e0f26f3842a9b2c1bb42378913c73
Elmeddin Xudaverdiyev <Xudaverdiyev11@gmail.com>
2023-09-20 16:34:36 +0400

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -86,52 +86,15 @@ accordionItems.forEach((accItem, accndex) => {
         }
     });
 });
-/*
-1. Her sliderin icinde kartdan elave displayi none olan bir image olsun. 
-2. Bir function olsun ve bu sehife yuklenende bu imageleri gotursun mockupin icinde yan yana duzsun ama birinci gorunen her zaman "swiper.activeIndex" olsun. 
-3. Bir function daha olsun ve burada "on('slideChange', ()=> {})" islesin. Bu function her defe slayd deyisdiyinde "swiper.activeIndex" e gore mockupin icindeki yan yana duran sekilleri transfrom.translateX ne faiz versin. Belelikle mockupin ici soldan saga ve ya tersi istiqametinde hereket edecek. Bundan once  "swiper.slides" (Bu slidelerin umumi sayini goturur) lengthini gotur ve 100u bu lengthe bol. Cixan deyeri "swiper.activeIndex"e vur, belelikle mockup icinde her sekil nece faiz saga sola hereket etmeli oldugunu hesablamis olacaq. 
-4. Saga ve ya sola hereket etmeli oldugunu ise "on('slidePrevTransitionStart', ()=> {})" ve "on('slideNextTransitionStart', ()=> {})" ile teyin et. Bir dene var teyin et, deyeri default olaraq "true" olsun, eger true-dursa "+", "false"-dursa "-" olsun. 
-*/
 
-// function myPlugin({ swiper, extendParams, on }) {
-//     extendParams({
-//         debugger: false,
-//     });
-
-//     on('slidePrevTransitionStart', () => {
-//         console.log("slidePrevTransitionStart")
-//     })
-//     on('slideNextTransitionStart', () => {
-//         console.log("slideNextTransitionStart")
-//     })
-//     on('slideChange', () => {
-//         if (!swiper.params.debugger) return;
-//         console.log(
-//             'slideChange',
-//             swiper.previousIndex,
-//             '->',
-//             swiper.activeIndex
-//         );
-//     });
-
-// }
-
-// Init Swiper
 var swiperBay = new Swiper('.swiperBay', {
     slidesPerView: 3.38719,
     autoHeight: true,
     spaceBetween: 24,
-    // autoplay: {
-    //   delay: 2500,
-    //   disableOnInteraction: false,
-    // },
-    // modules: [myPlugin],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    // Enable debugger
-    debugger: true,
     breakpoints: {
         320: {
             slidesPerView: 1.04,
@@ -176,7 +139,6 @@ var swiperBay = new Swiper('.swiperBay', {
 
 var swiper = new Swiper(".ValueSwiper", {
     slidesPerView: 2,
-    // spaceBetween: 96,
     pagination: false,
     grid: {
         rows: 2,
@@ -257,6 +219,51 @@ var swiper = new Swiper(".refSwiper", {
         },
         1024: {
             slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".userSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 32,
+    pagination: false,    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1.5,
+            spaceBetween: 16,
+        },
+        375: {
+            slidesPerView: 1.75,
+            spaceBetween: 16,
+        },
+        450: {
+            slidesPerView: 2,
+            spaceBetween: 16,
+        },
+        525: {
+            slidesPerView: 2.4,
+            spaceBetween: 16,
+        },
+        650: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+        },
+        768: {
+            slidesPerView: 3.25,
+            spaceBetween: 32,
+        },
+        900: {
+            slidesPerView: 3.5,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 4.5,
         },
     },
 });
